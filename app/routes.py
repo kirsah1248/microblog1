4bd6d9a5a66a6bf77a451f74cc5991ca08aff9eb
kirsah1248 <122039836+kirsah1248@users.noreply.github.com>
2023-02-25 15:09:17 -0500

    Chapter 7

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm, OtherForm, RegistrationForm
+from app.forms import LoginForm, OtherForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user
 from app.models import User
 from flask_login import login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -70,3 +71,24 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return render_template('user.html', user=current_user, posts=current_user.posts)
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',form=form)
